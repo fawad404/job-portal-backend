96380a0c6ef7c015a68cc429be2cd7f9cc827088
fawad404 <fawadanxari31@gmail.com>
2025-03-06 01:49:45 +0500

    added cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 // Middleware
 app.use(express.json())
 app.use(cors( {
-origin: ["http://localhost:5173"],
+origin: ["https://job-portal-fawad-application.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true
 }
